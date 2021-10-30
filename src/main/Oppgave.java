566e9821d84ce4032fbf19e6f775f41ae4a82f10
mouhamad5210 <60216949+mouhamad5210@users.noreply.github.com>
2021-10-31 01:20:18 +0200

    Add files via upload

---
+++ src/main/Oppgave.java
@@ -0,0 +1,11 @@
+package no.oslomet.cs.algdat.Oblig3;
+
+@FunctionalInterface
+public interface Oppgave<T>{  // legges under hjelpeklasser
+
+    void utførOppgave(T t);    // f.eks. utskrift til konsollet
+
+    
+}
+
+
